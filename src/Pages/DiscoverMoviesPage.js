7d9b94f1c7fe8e0e94d86968e7e67f2d51f7ae39
Johann Eugen <jekneto@hotmail.com>
2021-10-21 14:49:28 +0200

    the search bar was creat on discovermovies

--- src/Pages/DiscoverMoviesPage.js
+++ src/Pages/DiscoverMoviesPage.js
@@ -1,3 +1,22 @@
-export default function DiscoveMoviesPages() {
-  return <div> Now You Are At Discover Movies Page</div>;
+import { useState } from "react";
+
+export default function DiscoverMoviesPage() {
+  const [searchText, set_searchText] = useState("");
+
+  const search = () => {
+    console.log("TODO search movies for:", searchText);
+  };
+
+  return (
+    <div>
+      <h1>Discover some movies!</h1>
+      <p>
+        <input
+          value={searchText}
+          onChange={(e) => set_searchText(e.target.value)}
+        />
+        <button onClick={search}>Search</button>
+      </p>
+    </div>
+  );
 }
